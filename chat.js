0c5711ac459170266ac6dedcff5738d877c981c4
= <adamlawrencegriffin@gmail.com>
2023-05-24 16:20:24 -0600

    update(chat.html)included some comments

--- chat.js
+++ chat.js
@@ -1,24 +1,27 @@
 const userNameTitleThingy = document.querySelector(".user-title");
 userNameTitleThingy.innerText = localStorage.getItem("userName");
-
+/** 
 function getUserName() {
     return localStorage.getItem('userName') ?? 'Current User';
 }
+*/
 
 
 
-
-
-
-
-
-
+ 
+/** 
 setInterval(() => {
-    const score = Math.floor(Math.random() * 7000);
     const chatText = document.querySelector('#active-status');
     chatText.innerHTML =
       `<div class="event"><span class="user-name">Joe</span> is active</div>` + chatText.innerHTML;
   }, 10000);
 
+
+  let chat = 'Tim:';
+setInterval(() => {
+  chat = `${chat} Response lol sent at 11:55`;
+  document.querySelector('#messages').textContent = chat;
+}, 5000);
+*/
 
 
