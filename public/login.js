6ddbbf1b012f4a208da2c12b0112363b1e662c19
Adam Griffin <adamlawrencegriffin@gmail.com>
2023-06-02 01:01:36 -0600

    fixed(login) throws error when empty

--- public/login.js
+++ public/login.js
@@ -1,21 +1,30 @@
 function login() {
     const nameEl = document.querySelector("#name");
-    localStorage.setItem("userName", nameEl.value);
-    //post to backend
+    const username = nameEl.value; // Get the username value
+  
+    if (!username) {
+        const errorMessage = document.querySelector("#error-message");
+        errorMessage.style.display = "block"; // Show the error message
+        return;
+    }
+  
+    localStorage.setItem("userName", username);
+  
+    // Post to backend
     fetch('/api/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username }),
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username }), // Pass the username in the request body
     })
-    .then(response => response.json())
-    .then(data => {
+      .then(response => response.json())
+      .then(data => {
         console.log(data);
         window.location.href = "chat.html";
-    })
-    .catch(error => {
+      })
+      .catch(error => {
         console.error('Error:', error);
-    });
-}
-
+      });
+  }
+  
