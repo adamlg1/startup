a26af45d04cee4804e20584588213498b3845ec5
Adam Griffin <adamlawrencegriffin@gmail.com>
2023-06-08 19:18:12 -0600

    (database) add  necessary components like simon

--- database.js
+++ database.js
@@ -20,6 +20,28 @@ const userCollection = db.collection('user');
     process.exit(1);
   });
 
+  function getUser(email) {
+    return userCollection.findOne({ email: email });
+  }
+
+  function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+  }
+
+  async function createUser(email, password) {
+    // hash password before it goes to db
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+      email: email,
+      password: passwordHash,
+      token:uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+
+    return user;
+  }
+
 //make sure db is ready to add 
   async function prepDatabase() {
     try {
@@ -66,8 +88,11 @@ async function getTips() {
 }
 
 module.exports = {
+  getUser,
+  getUserByToken,
+  createUser,
   addMessage,
   getMessages,
   addTip,
-  getTips
+  getTips,
 };
