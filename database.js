53446f63bb1e22cea903735749ed30664ea8d385
Adam Griffin <adamlawrencegriffin@gmail.com>
2023-06-08 19:06:26 -0600

    (database) added userCollection

--- database.js
+++ database.js
@@ -1,4 +1,6 @@
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
@@ -6,6 +8,7 @@ const client = new MongoClient(url);
 const db = client.db('startup');
 const messagesCollection = db.collection('message');
 const tipsCollection = db.collection('tips');
+const userCollection = db.collection('user');
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
