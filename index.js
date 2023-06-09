b7285ef978bdc4c3d983208dbd030fec8f27e2ff
Adam Griffin <adamlawrencegriffin@gmail.com>
2023-06-08 19:03:49 -0600

    (index) got necessary endpoints from simon

--- index.js
+++ index.js
@@ -42,6 +42,50 @@ apiRouter.post('/auth/create', async (req, res) => {
   }
 });
 
+//GetAuth token for provided login
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser(req.body.email);
+  if (user) {
+    if(await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({id: user._id});
+      return;
+    }
+  }
+  res.status(401).end({msg: 'Unauthorized'});
+});
+
+//DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+//GetUser returns info about user
+apiRouter.get('/user/:email', async(req, res) => {
+  const user = await DB.getUser(req.params.email);
+  if (user) {
+    const token = req?.cookies.token;
+    res.send({ email: user.email, autenticated: token === user.token });
+    return;
+  }
+  res.status(404).send({ msg: 'Unknown' });
+});
+
+//secureApiRouter verifies credentials for endpoints
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized :('});
+  }
+});
+
 
 // Tips
 apiRouter.get('/tips', async (_req, res) => {
@@ -122,12 +166,27 @@ apiRouter.post('/login', (req, res) => {
   res.json({ user });
 });
 
+//default error handler (not sure if needed)
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
 
 // Return application's default page if path is unknown
 app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
 
+//setAuthCookie in HTTP response
+function setAuthCookie (res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
+//to see what port it is on
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
