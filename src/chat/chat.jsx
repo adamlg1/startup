d8f6ba1874090035e6c43afb7dc61e8c02c90668
Adam Griffin <adamlawrencegriffin@gmail.com>
2023-06-15 22:17:35 -0600

    Got HTML into all JSX files

--- src/chat/chat.jsx
+++ src/chat/chat.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import './chat.css';
+
 
 export function Chat() {
   return (
-    <main className='bg-light text-info text-center'>
-      <div>Chat displayed here</div>
-    </main>
+<main class="bg-light">
+    <span className="chat-title"><h1>Live Chat</h1></span>
+
+    
+    <div className="users">
+      User:
+      <span class="user-title"></span>
+      <div></div>
+      <span class="active-title"> Others Active:</span>
+      <div class="active-status" id="active-status">
+      </div>
+    </div>
+
+    
+    <div id="messages" className="message-container">
+      <span className="message-title"><h1>Messages:</h1></span>
+      <div className="anotherSpacerWooo">
+      </div>
+    </div>
+
+  
+    <div className="chat-input">
+      <input type="text" id="typed-message" name="varText" placeholder="type message here" />
+      <button id="send">send</button>
+    </div>
+  </main>
   );
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,39 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className='bg-light text-info text-center'>
-      <div>login displayed here</div>
-    </main>
+<main className="bg-light text-info text-center">
+      <div>
+        <h1>Welcome</h1>
+        <p id="intro">Let's talk about gluten</p>
+          <form id = "loginControls" style={{ display: 'none'}}>
+              <div className="form-group">
+                <label for="userName">Email address</label>
+                <input className = "form-control" type="text" id="userName" aria-describedby="emailHelp" placeholder="Enter email" />
+              </div>
+              <div className="form-group">
+                <label for="userPassword">Password</label>
+                <input type="password" className="form-control" id="userPassword" placeholder="Password" />
+              </div>
+                <p></p>
+              <button type="button" className="btn btn-primary" onclick="loginUser()">Login</button>
+              <button type="button" className="btn btn-primary" onclick="createUser()">Create</button>
+          </form>
+              <div id="chatControls" style={{ display: 'none' }}>
+                <div id="userName"></div>
+                <button type="button" className="btn btn-primary" onclick="chat()">Chat</button>
+                <button type="button" className="btn btn-secondary" onclick="logout()">Logout</button>
+              </div>
+      </div>
+        <div className="modal fade" id="msgModal" tabindex="-1">
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content text-dark">
+              <div className="modal-body">error message here</div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
   );
 }
