2679fd0de160ec8b868a0e9d7d6b64d8acb32b19
Adam Griffin <adamlawrencegriffin@gmail.com>
2023-06-19 00:19:51 -0600

    Fixed date error

--- src/chat/chat.jsx
+++ src/chat/chat.jsx
@@ -11,18 +11,19 @@ const Chat = () => {
 
   const retrieveTheTime = () => {
     const date = new Date();
-
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
+  
+    const day = String(date.getDate());
+    const month = String(date.toLocaleString('default', { month: 'long' }));
     const year = String(date.getFullYear());
     let hours = date.getHours();
     const minutes = String(date.getMinutes()).padStart(2, '0');
     const seconds = String(date.getSeconds()).padStart(2, '0');
     const amPm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12 || 12;
-
-    return `${day}/${month}/${year}, ${hours}:${minutes}:${seconds} ${amPm}`;
+  
+    return `${month} ${day}, ${year}, ${hours}:${minutes}:${seconds} ${amPm}`;
   };
+  
 
   async function fetchChatMessages() {
     try {
